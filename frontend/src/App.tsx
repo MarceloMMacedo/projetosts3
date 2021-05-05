bed2d2f353671c4e3cf25a04e67712373d546a0a
marcelo_macedo01@hotmail.com <marcelo_macedo01@hotmail.com>
2021-05-05 06:56:55 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,23 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
 
 function App() {
   return (
-    <div >
-      <h1 className="text-primary">olá word</h1>
-    </div>
+    <>
+      <div >
+        <NavBar />
+        <div className="container">
+          <h1 className="text-primary">marcelo</h1>
+          <DataTable />
+        </div>
+
+
+      </div>
+      <Footer />
+    </>
   );
+
 }
 
 export default App;
